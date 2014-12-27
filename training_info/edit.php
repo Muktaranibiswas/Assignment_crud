684c0df7d702fee2c64e68bcab3b68daf3ff5bd3
Muktaranibiswas <muktaquate@yahoo.com>
2014-12-27 13:02:59 -0500

    training_info update

--- training_info/edit.php
+++ training_info/edit.php
@@ -13,32 +13,32 @@ $row = mysqli_fetch_assoc($result);
 
     <label>Training Title :</label>
     <input type="text" name="ttitle" value="<?php echo $row['ttitle'];?>" />
-
+    <br>
     <label>Training Description:</label>
     <input type="text" name="tdescription" value="<?php echo $row['tdescription'];?>" />
-
+    <br>
     <label>Institute:</label>
     <input type="text" name="institute" value="<?php echo $row['institute'];?>" />
-
+    <br>
     <label>Address:</label>
     <input type="text" name="address" value="<?php echo $row['address'];?>" />
-
+    <br>
     <label>Training Year : </label>
     <input type="text" name="tyear" value="<?php echo $row['tyear'];?>" />
-
+    <br>
     <label>Duration (in days) : </label>
     <input type="text" name="duration" value="<?php echo $row['duration'];?>" />
-
+    <br>
     <label>Start Date:</label>
     <input type="text" name="sdate" value="<?php echo $row['sdate'];?>" />
-
+    <br>
     <label>End Date:</label>
     <input type="text" name="edate" value="<?php echo $row['edate'];?>" />
-
+    <br>
 
     <label>Course Title:</label>
     <input type="text" name="ctitle" value="<?php echo $row['ctitle'];?>" />
-
+    <br>
     <label>Trainer Detail:</label>
     <input type="text" name="tdetail" value="<?php echo $row['tdetail'];?>" />
     <br>
